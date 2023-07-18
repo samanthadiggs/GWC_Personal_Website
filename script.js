924b20d8f72eebc286d072ac46e786a2a5379868
samantha diggs <99908108+samanthadiggs@users.noreply.github.com>
2023-07-17 21:33:33 -0400

    got so much done!!!!

---
+++ script.js
@@ -0,0 +1,8 @@
+function reveal() {
+    var reveals = document.querySelectorAll('.reveal')
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisibile = 150;
+    }
+}
