df76a1882d9e0f78bae21016e31a3123eb1a3704
samantha diggs <99908108+samanthadiggs@users.noreply.github.com>
2023-07-18 16:02:27 -0400

    got the transition to work!!

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ function reveal() {
     for (var i = 0; i < reveals.length; i++) {
         var windowHeight = window.innerHeight;
         var elementTop = reveals[i].getBoundingClientRect().top;
-        var elementVisibile = 150;
+        var elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        }
+            else {
+                reveals[i].classList.remove("active")
+            }
+        }
     }
-}
+
+    window.addEventListener("scroll", reveal);
+    reveal()
