1f1e67c2fbbf9603e1b83e44d9cc412c8d8d8988
samantha diggs <99908108+samanthadiggs@users.noreply.github.com>
2023-07-19 13:12:08 -0400

    added more transitions <3

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+/* page transition function  */
 function reveal() {
     var reveals = document.querySelectorAll('.reveal')
     for (var i = 0; i < reveals.length; i++) {
@@ -15,3 +17,40 @@ function reveal() {
 
     window.addEventListener("scroll", reveal);
     reveal()
+
+    /* left side reveal */
+    function reveal2() {
+        var reveals = document.querySelectorAll('.reveal2')
+        for (var i = 0; i < reveals.length; i++) {
+            var windowHeight = window.innerHeight;
+            var elementTop = reveals[i].getBoundingClientRect().top;
+            var elementVisible = 150;
+            if (elementTop < windowHeight - elementVisible) {
+                reveals[i].classList.add("active");
+            }
+                else {
+                    reveals[i].classList.remove("active")
+                }
+            }
+        }
+
+window.addEventListener("onload", reveal2)
+reveal2()
+
+function reveal3() {
+    var reveals = document.querySelectorAll('.reveal3')
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        }
+            else {
+                reveals[i].classList.remove("active")
+            }
+        }
+    }
+
+window.addEventListener("onload", reveal3)
+reveal3()
